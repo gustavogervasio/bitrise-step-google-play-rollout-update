0570681ee5f7a9cefad917f61d50ec8892df7fb1
Havel <eric.havel@gmail.com>
2024-11-06 06:49:14 -0300

    fix: Investigando erros do bitrise

--- step.sh
+++ step.sh
@@ -3,6 +3,9 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Identificador para ficar claro qual versão o bitrise está rodando
+echo "Running step.sh 0.0.1"
+
 echo "Updating phase release for: ${package_name}"
 if [ -z "$service_account_json_key_content" ] ; then
     echo "Downloading credentials from remote file"
@@ -12,9 +15,13 @@ else
     echo "$service_account_json_key_content" > "${SCRIPT_DIR}/credentials.json"
 fi
 
-pipenv install google-api-python-client==2.148.0
+echo "Installing google-api-python-client"
+pipenv install google-api-python-client
+pipenv run python -c "import googleapiclient; print(googleapiclient.__version__)"
+
+echo "Installing oauth2client"
 pipenv install oauth2client
-pipenv install urllib3
+pipenv run python -c "import oauth2client; print(oauth2client.__version__)"
 
 pipenv run python "${SCRIPT_DIR}/rollout_update.py" "${package_name}" "${SCRIPT_DIR}/credentials.json"
 
